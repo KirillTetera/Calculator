1902172775d1b2c35dca6db5c92906a89ab7366c
kvtetera <kirill.v.tetera@gmail.com>
2023-02-12 00:07:03 +0300

    refactor java classes

--- src/main/java/calculatorMain.java
+++ src/main/java/calculatorMain.java
@@ -1,9 +1,13 @@
 public class calculatorMain {
     public static void main(String[] args) {
         calculatorData calculator = new calculatorData();
-        calculator.calculatorSum(5,10);
-        calculator.calculatorSubtruct(10,20);
-        calculator.calculatorDevide(30,40);
-        calculator.calculatorMultiply(50,60);
+        double res1 = calculator.calculatorSum(5,10);
+        double res2 = calculator.calculatorSubtruct(10,20);
+        double res3 = calculator.calculatorDevide(30,40);
+        double res4 = calculator.calculatorMultiply(50,60);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
     }
 }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -1,15 +1,29 @@
-import com.sun.org.apache.xpath.internal.operations.Equals;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class calculatorTest {
-    public static void main(String[] args) {
         @Test
+        public void testSum(){
         calculatorData test = new calculatorData();
         double actual = test.calculatorSum(5,10);
-        Assert.assertEquals(15,actual);
-
-
-
-    }
+        Assert.assertEquals(15, actual,0.01);
+        }
+        @Test
+        public void testSub(){
+                calculatorData test = new calculatorData();
+                double actual = test.calculatorSubtruct(5.5,10.5);
+                Assert.assertEquals(-5.0, actual,0.01);
+        }
+        @Test
+        public void testMultyply(){
+                calculatorData test = new calculatorData();
+                double actual = test.calculatorMultiply(2,2);
+                Assert.assertEquals(4, actual,0.01);
+        }
+        @Test
+        public void testDevide(){
+                calculatorData test = new calculatorData();
+                double actual = test.calculatorDevide(6,2);
+                Assert.assertEquals(3, actual,0.01);
+        }
 }
